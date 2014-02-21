646673b135fa6794b615ad2f73f4a1d6cd936ef8
Bill Pullen <bill.pull@gmail.com>
2014-02-21 00:20:19 -0500

    Update xignite.py

--- snaf/xignite.py
+++ snaf/xignite.py
@@ -1,7 +1,7 @@
 import urllib2
 import simplejson
 
-XIGNITE_TOKEN = "01982C1733DF487A9F5127F1AE36C412"
+XIGNITE_TOKEN = "XIGNITE_API_TOKEN"
 XIGNITE_ENDPOINT = "http://www.xignite.com/xEarningsCalendar.json/GetAnnouncement?Identifier=%s&IdentifierType=Symbol&_Token=%s"
 
 def get_xignite_events(symbol):
